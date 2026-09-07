ed782c5a3fe1ee724f69b7f2e698215b83cf1fa7
noah <noah@noahdeMacBook-Air.local>
2026-09-07 11:30:55 +0800

    修复错误

--- src/utils/verify/__init__.py
+++ src/utils/verify/__init__.py
@@ -3,13 +3,11 @@
 from utils.verify.instruction import (
     MAX_INSTRUCTION_LEN,
     SENTENCE_SEPARATORS,
-    VERB_PREFIXES,
     validate_instruction,
 )
 
 __all__ = [
     "MAX_INSTRUCTION_LEN",
     "SENTENCE_SEPARATORS",
-    "VERB_PREFIXES",
     "validate_instruction",
 ]
